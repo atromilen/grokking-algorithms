f3470a5ee2e962a20dbf72fac9a8a8142e22c032
Álvaro Tromilen <alvaro.tromilen@consorcio.cl>
2021-04-26 02:28:09 -0400

    Comment correction

--- chap4_1_divide_and_conquer.py
+++ chap4_1_divide_and_conquer.py
@@ -6,7 +6,7 @@ def squarePlotsDivision(biggest, lowest):
     else:
         return squarePlotsDivision(lowest, biggest % lowest)
 
-# In this example, we want to divide a plot of 1680 x 640 metters into the most small squires possible.
+# In this example, we want to divide a plot of 1680 x 640 metters into squares as big as possible.
 # Using Divide & Conquer, we figure out what are the biggest and smallest boxes we can use to divide:
 # We need to know that is the smaller box to complete the full 1680 meassurement. 
 # i.e. 1680 = 640 + 640 + 400 (this last is smallest box than we can use) 
@@ -17,7 +17,7 @@ def squarePlotsDivision(biggest, lowest):
 # This solution is bassed on Euclide´s algorithm:
 # https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm 
 
-biggest = 1680
-lowest = 640
+biggest = 50
+lowest = 50
 square = squarePlotsDivision(biggest, lowest)
 print('Smaller square for a plot of {}x{}: {}'.format(biggest, lowest, square))
